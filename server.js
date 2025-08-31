345212cdd0a2ed9ef7e8cffbd8aab45f379f1e1a
temptationkentos57 <kreetlannoo@hotmail.com>
2025-08-31 17:11:32 +0200

    Update MongoDB connection string to use 'mongodb+srv://' format for Atlas compatibility.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/quanlylamviec', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/quanlylamviec?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Kết nối với MongoDB thành công'))
   .catch(err => console.error('Lỗi kết nối MongoDB:', err));
 
